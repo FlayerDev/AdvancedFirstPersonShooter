8ee2b1ded189d5474b9166b9b2675d72ed9f7777
FlayerDev <flayer.dev.gr@gmail.com>
2021-04-03 00:48:59 +0300

    Implemented FP Hands, Kinda

--- Assets/Behaviour/Player/CameraMounter.cs
+++ Assets/Behaviour/Player/CameraMounter.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraMounter : MonoBehaviour
 {
     public Camera MainCamera;
+    public GameObject FP_Hands;
+    public Vector3 HandOffset;
+    public bool Focused;
 
     private void Awake()
     {
@@ -14,6 +17,21 @@ public class CameraMounter : MonoBehaviour
     public void Start()
     {
         if (!transform.parent.GetComponent<Mirror.NetworkIdentity>().isLocalPlayer) return;
+        Focus(); //TODO: Implement unfocus or hide FP Hands when not focused
+    }
+    private void Update()
+    {
+        if (transform != MainCamera.transform.parent)
+        {
+            Focused = false;
+            return;
+        }
+        FP_Hands.transform.position = MainCamera.transform.position + HandOffset; // Note: this doesnt work!
+        FP_Hands.transform.rotation = MainCamera.transform.rotation;
+    }
+    public void Focus()
+    {
+        Focused = true;
         MainCamera.transform.parent = transform;
         transform.parent.GetComponent<CameraRotation>().playerBody = transform.parent.gameObject;
         transform.parent.GetComponent<CameraRotation>().cameraObj = MainCamera.gameObject;
