8924da6b87672f93294aa5d8eec2133e15ee777b
FlayerDev <flayer.dev.gr@gmail.com>
2023-12-08 12:05:43 +0200

    Added Team selection
    TODO
    - Spawn code to be moved from PlayerSpawner.cs to a script on the actual player (nullref on line 50)
    - Implement spectator

--- Assets/Behaviour/World/PlayerSpawner.cs
+++ Assets/Behaviour/World/PlayerSpawner.cs
@@ -21,16 +21,33 @@ public class PlayerSpawner : NetworkBehaviour
 
     public void SelectTeam(int team)
     {
-        Instantiate( team == 0? LobbyManager.Singleton.CT_Player_Prefab: LobbyManager.Singleton.T_Player_Prefab, GetSpawnPosition((Team)team, 1), Quaternion.identity);
         CmdSelectTeam(team);
-        LocalInfo.Observer.SetActive(false);
+        //LocalInfo.Observer.SetActive(false);
         TeamSelectionHUD.SetActive(false);
+        //LocalInfo.localIdentity.gameObject.GetComponent<PlayerInfo>().playerTeam = team;
     }
 
-    [Command]
+    [Command(ignoreAuthority =true)]
     public void CmdSelectTeam(int team)
     {
-        LocalInfo.localIdentity.gameObject.GetComponent<PlayerInfo>().playerTeam = team;
+        GameObject pref = LobbyManager.Singleton.playerPrefab;
+        switch (team)
+        {
+            case 0:
+                pref = LobbyManager.Singleton.CT_Player_Prefab;
+                break;
+            case 1:
+                pref = LobbyManager.Singleton.T_Player_Prefab;
+                break;
+            default:
+                pref = LobbyManager.Singleton.playerPrefab;
+                break;
+        }
+        Debug.Log(pref);
+        var gO = Instantiate(pref, GetSpawnPosition((Team)team, 1), Quaternion.identity);
+        Debug.Log(gO);
+        NetworkServer.Spawn(gO, this.connectionToClient);
+        NetworkServer.ReplacePlayerForConnection(this.connectionToClient, gO);
     }
 
     public Vector3 GetSpawnPosition(Team team, int seed)
@@ -38,7 +55,7 @@ public class PlayerSpawner : NetworkBehaviour
         switch (team)
         {
             case Team.CT:
-                return CT_Spawner.transform.GetChild(Math.Clamp( seed,0, CT_Spawner.transform.childCount)).position;
+                return CT_Spawner.transform.GetChild(Math.Clamp(seed, 0, CT_Spawner.transform.childCount)).position;
             case Team.T:
                 return T_Spawner.transform.GetChild(Math.Clamp(seed, 0, T_Spawner.transform.childCount)).position;
         }
