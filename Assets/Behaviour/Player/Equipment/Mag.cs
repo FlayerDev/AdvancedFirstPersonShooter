eac3c73f7400debe71eb56fa94242b9e0b673849
FlayerDev <flayer.dev.gr@gmail.com>
2023-12-19 02:16:34 +0200

    Made Reload Cancellable

--- Assets/Behaviour/Player/Equipment/Mag.cs
+++ Assets/Behaviour/Player/Equipment/Mag.cs
@@ -1,17 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
 using UnityEngine;
 using Mirror;
 
 [AddComponentMenu("Flayer/Combat/Mag")]
 public class Mag : NetworkBehaviour
 {
+    /// <summary>
+    /// This is the instance of the held magazine
+    /// </summary>
+    public static Mag MagSingleton; 
+
     [SyncVar] public int Ammo = 30;
     [SyncVar] public int InventoryAmmo = 90;
     public int Capacity = 30;
     public int InventoryCapacity = 90;
     public int ReloadAmount = 30;
     public int ReloadTimeMS = 2500;
+
+    CancellationTokenSource reloadCancellationToken;
+    bool reloaded = false;
+
+    private void OnEnable()
+    {
+        if (GetComponentInParent<Inventory>().isLocalInventory) MagSingleton = this;
+    }
+    private void OnDisable()
+    {
+        if (reloadCancellationToken != null)
+            try { reloadCancellationToken.Cancel(); } catch { print("Mag:CouldNotCancelReload"); }
+
+        if (GetComponentInParent<Inventory>().isLocalInventory) MagSingleton = null;
+    }
+
+    private void Update()
+    {
+        if (reloaded)
+        {
+            reloaded = false;
+            var ammobuffer = Ammo;
+            CmdSetAmmo(Mathf.Clamp(Ammo + Mathf.Clamp(ReloadAmount, 0, InventoryAmmo), 0, Capacity));
+            CmdSetInvAmmo(InventoryAmmo - (ReloadAmount - ammobuffer));
+        }
+    }
 
     [Command(ignoreAuthority = true)]
     public void CmdSetAmmo(int val)
@@ -25,11 +59,30 @@ public class Mag : NetworkBehaviour
         Debug.Log(val);
         InventoryAmmo = val;
     }
-    public async void Reload()
+    public void StartReload()
     {
-        await System.Threading.Tasks.Task.Delay(ReloadTimeMS);
-        var ammobuffer = Ammo;
-        CmdSetAmmo(Mathf.Clamp(Ammo + Mathf.Clamp(ReloadAmount, 0, InventoryAmmo), 0, Capacity));
-        CmdSetInvAmmo(InventoryAmmo - (ReloadAmount - ammobuffer));
+        reloadCancellationToken = new CancellationTokenSource();
+        Task.Run(async () =>
+            {
+                await Task.Delay(ReloadTimeMS);
+                if (!reloadCancellationToken.Token.IsCancellationRequested) reloaded = true;
+            }
+        );
+    }
+    public void CancelReload()
+    {
+        try
+        {
+            reloadCancellationToken.Cancel();
+            print("Mag:ReloadCancelled");
+        }
+        catch (NullReferenceException)
+        { 
+            print("Mag:CouldNotCancelReload(no_active_reload)"); 
+        }
+        catch (Exception ex)
+        {
+            print($"Mag:CouldNotCancelReload({ex})");
+        }
     }
 }
